d3263bd5d78de68908127e1c032d35153a9803e3
Kyle Yannelli <kyleyannelli@gmail.com>
2023-07-10 13:11:51 -0400

    Added some languages and utils to cheat-sheet

--- cht.sh
+++ cht.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-languages=`echo "java cpp golang bash zsh shell" | tr ' ' '\n'`
-core_utils=`echo "find sed echo printf awk" | tr ' ' '\n'`
+languages=`echo "rust docker java python cpp golang bash zsh shell" | tr ' ' '\n'`
+core_utils=`echo "git find sed tmux echo printf awk" | tr ' ' '\n'`
 
 selected=`printf "$languages\n$core_utils" | fzf`
 read -p "query: " query
@@ -10,5 +10,5 @@ window_count=$(tmux list-windows | wc -l)
 if printf $languages | grep -qs $selected; then
 	tmux neww -t $window_count -n $selected/$query zsh -c "curl cht.sh/$selected/$(echo "$query" | tr ' ' '+') & while [ : ]; do sleep 1; done"
 else
-	tmux neww -t $window_count -n $selected/$query zsh -c "curl -s cht.sh/$selected~$query | less"
+	tmux neww -t $window_count -n $selected/$query zsh -c "curl -s cht.sh/$selected~$(echo "$query" | tr ' ' '+') & while [ : ]; do sleep 1; done"
 fi
